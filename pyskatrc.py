a6757dc5ff8985e917d4224666088d9d1ed816d8
kongo2002 <kongo2002@googlemail.com>
2010-11-09 02:19:07 +0100

    add license

--- pyskatrc.py
+++ pyskatrc.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
-# Last Change:  Sa 06 Feb 2010 18:37:29 CET
+
+#  This file is part of pyskat.
+#
+#  Copyright (C) 2010 by Gregor Uhlenheuer
+#
+#  pyskat is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  pyskat is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with pyskat.  If not, see <http://www.gnu.org/licenses/>.
 
 KARO = 40
 HERZ = 60
