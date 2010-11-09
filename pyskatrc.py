cc86984c9d5dd079eff889d4afd3146f205ffd96
kongo2002 <kongo2002@googlemail.com>
2010-11-09 02:25:02 +0100

    fix license

--- pyskatrc.py
+++ pyskatrc.py
@@ -2,7 +2,7 @@
 
 #  This file is part of pyskat.
 #
-#  Copyright (C) 2010 by Gregor Uhlenheuer
+#  Copyright (C) 2009, 2010 by Gregor Uhlenheuer
 #
 #  pyskat is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
